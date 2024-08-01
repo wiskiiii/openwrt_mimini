6d8ec272d17eb57890321b8ddca556ee2d29bff0
wiskiiii <123956465+wiskiiii@users.noreply.github.com>
2024-08-01 16:52:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,4 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
